493a9b243393ac56f6ef2b1803f663514aa3109e
Ivano Bilenchi <ivanobilenchi@gmail.com>
2019-03-02 15:57:51 +0100

    Stop auto-scroller on touch

--- js/auto_scroll.js
+++ js/auto_scroll.js
@@ -1,57 +1,85 @@
 // Configuration
 
-const AS_THRESHOLD = 250.0
-const AS_SPEED = .03
+const AS_SCROLL_END_TIMEOUT = 500.0;
+const AS_THRESHOLD = 250.0;
+const AS_SPEED = .03;
 
 // Functions
 
 function autoScrollStart(element) {
-	autoScrollStop(element)
+  if (element.isAutoScrolling) return;
 
-	const leftToRight = element.autoScrollLeftToRight
+  element.isAutoScrolling = true;
+  const leftToRight = element.autoScrollLeftToRight;
 
-	const scrollStart = 0
-	const scrollEnd = element.scrollWidth - element.clientWidth
+  const scrollStart = 0;
+  const scrollEnd = element.scrollWidth - element.clientWidth;
 
-	const newPosition = leftToRight ? scrollEnd : scrollStart
-	const duration = Math.abs(newPosition - element.scrollLeft) / AS_SPEED
+  const newPosition = leftToRight ? scrollEnd : scrollStart;
+  const duration = Math.abs(newPosition - element.scrollLeft) / AS_SPEED;
 
-	$(element).animate({ scrollLeft: newPosition },
-	{
-		duration: duration,
-		complete: function() {
-			element.autoScrollLeftToRight = !leftToRight
-			autoScrollStart(element)
-		}
-	});
+  $(element).animate({ scrollLeft: newPosition },
+  {
+    duration: duration,
+    complete: () => {
+      element.autoScrollLeftToRight = !leftToRight;
+      element.isAutoScrolling = false;
+      autoScrollStart(element);
+    }
+  });
 }
 
 function autoScrollStop(element) {
-	$(element).stop()
+  if (!element.isAutoScrolling) return;
+
+  element.isAutoScrolling = false;
+  $(element).stop();
 }
 
 function autoScrollInit(element) {
-	element.autoScrollLeftToRight = true
-	$(element).hover(
-		function() { autoScrollStop(element) },
-		function() { autoScrollStart(element) }
-	)
-	autoScrollStart(element)
+  element.isAutoScrolling = false;
+  element.autoScrollLeftToRight = true;
+
+  $(element).hover(
+    () => autoScrollStop(element),
+    () => autoScrollStart(element)
+  );
+
+  $(element).on('touchmove', () => {
+    autoScrollStop(element);
+  });
+
+  $(element).on('touchend', () => {
+    $(element).on('scrollEnded', () => {
+      $(element).off('scrollEnded');
+      autoScrollStart(element);
+    });
+  });
+
+  $(element).scroll(() => {
+    if (element.timer) clearTimeout(element.timer);
+
+    element.timer = setTimeout(() => {
+      $(element).trigger('scrollEnded');
+    }, AS_SCROLL_END_TIMEOUT);
+  });
+
+  autoScrollStart(element);
 }
 
 function autoScrollDeinit(element) {
-	$(element).hover(null, null)
-	autoScrollStop(element)
+  $(element).hover(null, null);
+  autoScrollStop(element);
 }
 
 function autoScrollInitAll() {
-	$(".auto-scroll").each(function() {
-		if (this.scrollWidth - this.clientWidth > AS_THRESHOLD) {
-			autoScrollInit(this)
-		} else {
-			autoScrollDeinit(this)
-		}
-	})
+  $(".auto-scroll").each(function() {
+    if (this.scrollWidth - this.clientWidth > AS_THRESHOLD) {
+      autoScrollInit(this);
+    } else {
+      autoScrollDeinit(this);
+    }
+  });
 }
 
 // Hooks
