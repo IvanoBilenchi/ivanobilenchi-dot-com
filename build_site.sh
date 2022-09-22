25633ed03501378d0b8cfb1473b9c5fe31cc944c
Ivano Bilenchi <ivanobilenchi@gmail.com>
2022-09-22 10:40:32 +0200

    Update build script to copy over .map files

--- build_site.sh
+++ build_site.sh
@@ -6,9 +6,9 @@ CSS_DIR="${ROOT_DIR}/css"
 JS_DIR="${ROOT_DIR}/js"
 
 # Copy over js libraries
-cp -f "${NODE_DIR}/jquery/dist/jquery.min.js" "${JS_DIR}"
-cp -f "${NODE_DIR}/bootstrap/dist/js/bootstrap.bundle.min.js" "${JS_DIR}"
-cp -f "${NODE_DIR}/isotope-layout/dist/isotope.pkgd.min.js" "${JS_DIR}"
+cp -f "${NODE_DIR}/jquery/dist/jquery.min."* "${JS_DIR}"
+cp -f "${NODE_DIR}/bootstrap/dist/js/bootstrap.bundle.min."* "${JS_DIR}"
+cp -f "${NODE_DIR}/isotope-layout/dist/isotope.pkgd.min."* "${JS_DIR}"
 
 # Compile css
 sass --no-source-map "${SASS_DIR}/style.scss" "${CSS_DIR}/style.css"
